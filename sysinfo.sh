6533f11510aa4d06678634aae26c5fd1f71247af
zhai0122 <im.zhai@portonmail.com>
2022-07-29 21:15:34 +0800

    add sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -3,6 +3,8 @@
 # Author: zhai0122
 # date: 2022/07/29
 # Description: 显示一些系统信息
+
+
 uptime | awk '{printf("System load: %.2f\t", $(NF-2))} NR==1{printf("Up: %s %s\t",$3,$4)} NR==1{printf("Users logged in: %s\n",$(NF-6))}'
 ps -aux | wc -l | awk '{printf("Processes: %d\n", $1)}'
 free -m | awk 'NR==2{printf("Memory usage: %s/%sMB %.2f%%\n", $3,$2,$3*100/$2)} NR==3{printf("Swap usage: %s/%sMB %.2f%%\n",$3,$2,$3*100/$2)}'
